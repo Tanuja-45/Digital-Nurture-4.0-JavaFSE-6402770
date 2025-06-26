1e91268a65e4c4e6bfaac608d7e82fa5e8a28e54
Thanuja Tummalacherla <162697474+Tanuja-45@users.noreply.github.com>
2025-06-26 15:00:15 +0530

    Update scenario2.sql

--- Week-2/PLSQL/Stored procedures/scenario2.sql
+++ Week-2/PLSQL/Stored procedures/scenario2.sql
@@ -1,10 +1,7 @@
 DELIMITER **
-CREATE PROCEDURE UpdateEmployeeBonus( IN p_department VARCHAR(50), IN p_bonus_percent DECIMAL(5,2)
-)
+CREATE PROCEDURE UpdateEmployeeBonus( IN p_department VARCHAR(50), IN p_bonus_percent DECIMAL(5,2))
 BEGIN
-    UPDATE Employees
-    SET Salary = Salary +(Salary * p_bonus_percent / 100)
-    WHERE Department =p_department;
+    UPDATE Employees SET Salary = Salary +(Salary * p_bonus_percent / 100) WHERE Department =p_department;
 END;
 **
 DELIMITER;
