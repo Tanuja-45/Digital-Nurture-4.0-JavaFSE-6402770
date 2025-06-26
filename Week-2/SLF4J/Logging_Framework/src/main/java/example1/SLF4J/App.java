9f486762634a8d3c148c65bb6f80bac804688121
Thanuja Tummalacherla <162697474+Tanuja-45@users.noreply.github.com>
2025-06-26 14:58:25 +0530

    Add files via upload

---
+++ Week-2/SLF4J/Logging_Framework/src/main/java/example1/SLF4J/App.java
@@ -0,0 +1,13 @@
+package example1.SLF4J;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
