0b7749d7d1a0662786cff95b5b80523c7793de75
Thanuja Tummalacherla <162697474+Tanuja-45@users.noreply.github.com>
2025-06-29 15:47:24 +0530

    Delete Week-2/JUnit_Testing directory

--- Week-2/JUnit_Testing/Setting up JUnit/Exercise-1/src/main/java/example/Exercise_1/App.java
+++
@@ -1,9 +0,0 @@
-package example.Exercise_1;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
--- Week-2/JUnit_Testing/Setting up JUnit/Exercise-1/src/main/java/example/Exercise_1/Calculator.java
+++
@@ -1,7 +0,0 @@
-package example.Exercise_1;
-
-public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
-}
